3f968d4b7216fcd9d176836657942a8fe98d15d6
durraizshahid <durraizshahid99@gmail.com>
2026-07-13 01:10:09 +0500

    Add encrypted-tbn0.gstatic.com to allowed image hosts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "media-www.canadiantire.ca",
       },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
     ],
   },
   webpack: (config) => {
